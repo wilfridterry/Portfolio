177c8cbb8945f5c9be51b17127d637f20844dce8
Stannis <zalevschi1998@gmail.com>
2022-06-11 00:56:27 +0300

    Fixed initiation of project

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 const gulp        = require('gulp');
 const browserSync = require('browser-sync');
-const sass        = require('gulp-sass');
+const sass        = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
-const imagemin = require('gulp-imagemin');
+// const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
 
 gulp.task('server', function() {
@@ -63,7 +63,7 @@ gulp.task('icons', function () {
 
 gulp.task('images', function () {
     return gulp.src("src/img/**/*")
-        .pipe(imagemin())
+        // .pipe(imagemin())
         .pipe(gulp.dest("dist/img"))
         .pipe(browserSync.stream());
 });
