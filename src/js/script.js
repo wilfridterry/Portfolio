1d5e42386e38f0eeac4705e962d22128e03b83b5
Stannis <zalevschi1998@gmail.com>
2022-06-14 00:43:03 +0300

    Finished the fourth section

--- src/js/script.js
+++ src/js/script.js
@@ -8,4 +8,17 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.menu__close').addEventListener('click', e => {
         menu.classList.toggle('active');
     });
+
+    configurepercentageSkills();
 });
+
+function configurepercentageSkills() {
+    const items = document.querySelectorAll('.skills__progress-item');
+
+    items.forEach(el => {
+        const percentage = el.querySelector('.skills__progress-persantage');
+        const result = el.querySelector('.skills__progress-result');
+
+        result.style.width = percentage.dataset.value + '%';
+    });
+}
